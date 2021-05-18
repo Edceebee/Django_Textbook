00c4a11f91fbb166877f59d07cf77b54d3121d02
Chinonso Edozie <edceebee@gmail.com>
2021-05-18 17:14:47 +0100

    contd of proj

--- TextbookProject1/settings.py
+++ TextbookProject1/settings.py
@@ -114,9 +114,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
